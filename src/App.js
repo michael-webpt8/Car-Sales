98df367352d9f413936d700da981ae18006181ba
Mikey Williams <manicdesigner@gmail.com>
2019-10-23 14:43:12 -0400

    add the dispatch of addFeature to buyItem

--- src/App.js
+++ src/App.js
@@ -4,40 +4,37 @@ import Header from './components/Header';
 import AddedFeatures from './components/AddedFeatures';
 import AdditionalFeatures from './components/AdditionalFeatures';
 import Total from './components/Total';
+import { addFeature } from './actions/cars';
 
-const App = () => {
-  const removeFeature = item => {
-    // dispatch an action here to remove an item
-  };
+const App = (props) => {
+	const removeFeature = (item) => {
+		// dispatch an action here to remove an item
+	};
 
-  const buyItem = item => {
-    // dipsatch an action here to add an item
-  };
+	const buyItem = (item) => {
+		// dispatch an action here to add an item
+		props.addFeature(item);
+	};
 
-  return (
-    <div className='boxes'>
-      <div className='box'>
-        <Header />
-        <AddedFeatures /*car={state.car} */ />
-      </div>
-      <div className='box'>
-        <AdditionalFeatures /*additionalFeatures={state.additionalFeatures} */
-        />
-        <Total /*car={state.car} additionalPrice={state.additionalPrice}*/ />
-      </div>
-    </div>
-  );
-};
-
-const mapStateToProps = state => {
-  additionalFeatures: state.additionalFeatures;
+	return (
+		<div className="boxes">
+			<div className="box">
+				<Header />
+				<AddedFeatures /*car={state.car} */ />
+			</div>
+			<div className="box">
+				<AdditionalFeatures />
+				<Total /*car={state.car} additionalPrice={state.additionalPrice}*/ />
+			</div>
+		</div>
+	);
 };
 
 const mapDispatchToProps = {
-  addFeature
+	addFeature
 };
 
 export default connect(
-  mapStateToProps,
-  mapDispatchToProps
+	null,
+	mapDispatchToProps
 )(App);
