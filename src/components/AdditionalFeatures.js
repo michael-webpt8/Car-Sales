4459c66d576d3cf9b011601f104cdecc2519eef2
Michael Williams <manicdesigner@gmail.com>
2019-10-22 20:26:17 -0400

    map additionalFeatures from state through connect react-redux

--- src/components/AdditionalFeatures.js
+++ src/components/AdditionalFeatures.js
@@ -1,12 +1,14 @@
 import React from 'react';
+import { connect } from 'react-redux';
+
 import AdditionalFeature from './AdditionalFeature';
 
 const AdditionalFeatures = props => {
   return (
-    <div className="content">
+    <div className='content'>
       <h4>Additional Features</h4>
       {props.additionalFeatures.length ? (
-        <ol type="1">
+        <ol type='1'>
           {props.additionalFeatures.map(item => (
             <AdditionalFeature key={item.id} feature={item} />
           ))}
@@ -18,4 +20,13 @@ const AdditionalFeatures = props => {
   );
 };
 
-export default AdditionalFeatures;
+const mapStateToProps = state => {
+  return {
+    additionalFeatures: state.additionalFeatures
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  {}
+)(AdditionalFeatures);
